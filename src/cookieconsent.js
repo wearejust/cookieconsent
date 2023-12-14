c3a7022576770e680d69e2fdac379b13129e46a4
Cees van Egmond <cees.vanegmond@wearejust.com>
2023-12-14 14:21:58 +0100

    Chore: Update

---
+++ src/cookieconsent.js
@@ -0,0 +1,27 @@
+import * as CookieConsent from "vanilla-cookieconsent"
+import { defaultConfig } from "./defaultConfig";
+
+const mergeConfigs = (defaultConfig, userConfig) => {
+    const mergedConfig = { ...defaultConfig };
+
+    for (const key in userConfig) {
+        if (userConfig.hasOwnProperty(key)) {
+            if (typeof userConfig[key] === 'object' && userConfig[key] !== null && defaultConfig[key]) {
+                mergedConfig[key] = mergeConfigs(defaultConfig[key], userConfig[key]);
+            } else {
+                mergedConfig[key] = userConfig[key];
+            }
+        }
+    }
+
+    return mergedConfig;
+}
+
+// Export your custom run method
+export const run = async (userConfig = {}) => {
+    await CookieConsent.run(mergeConfigs(defaultConfig, userConfig));
+};
+
+export const acceptService = CookieConsent.acceptCategory;
+export const acceptedCategory = CookieConsent.acceptedCategory;
+export const acceptedService = CookieConsent.acceptedService;
