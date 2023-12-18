7e631a7905014c38df0aacb5a22e48a69bf020c8
Cees van Egmond <cees.vanegmond@wearejust.com>
2023-12-18 21:09:13 +0100

    Chore: Update

--- src/cookieconsent.js
+++ src/cookieconsent.js
@@ -4,27 +4,38 @@ import { defaultConfig } from "./defaultConfig";
 const mergeConfigs = (defaultConfig, userConfig) => {
     const mergedConfig = { ...defaultConfig };
 
-    for (const key in userConfig) {
-        if (userConfig.hasOwnProperty(key)) {
-            if (typeof userConfig[key] === 'object' && userConfig[key] !== null && defaultConfig[key]) {
-                mergedConfig[key] = mergeConfigs(defaultConfig[key], userConfig[key]);
-            } else {
-                mergedConfig[key] = userConfig[key];
-            }
-        }
+    for (const [key, userValue] of Object.entries(userConfig)) {
+        const isObject = typeof userValue === 'object' && userValue !== null && !Array.isArray(userValue);
+        const hasDefault = defaultConfig.hasOwnProperty(key);
+
+        mergedConfig[key] = isObject && hasDefault
+            ? mergeConfigs(defaultConfig[key], userValue)
+            : userValue;
     }
 
     return mergedConfig;
-}
+};
 
 // Export your custom run method
 export const run = async (userConfig = {}) => {
     await CookieConsent.run(mergeConfigs(defaultConfig, userConfig));
 };
 
-export const acceptService = CookieConsent.acceptCategory;
+export const acceptCategory = CookieConsent.acceptCategory;
 export const acceptedCategory = CookieConsent.acceptedCategory;
 export const acceptedService = CookieConsent.acceptedService;
+export const acceptService = CookieConsent.acceptService;
+export const eraseCookies = CookieConsent.eraseCookies;
+export const getConfig = CookieConsent.getConfig;
+export const getCookie = CookieConsent.getCookie;
 export const getUserPreferences = CookieConsent.getUserPreferences;
+export const hide = CookieConsent.hide;
+export const hidePreferences = CookieConsent.hidePreferences;
+export const loadScript = CookieConsent.loadScript;
+export const reset = CookieConsent.reset;
+export const setCookieData = CookieConsent.setCookieData;
+export const setLanguage = CookieConsent.setLanguage;
 export const show = CookieConsent.show;
 export const showPreferences = CookieConsent.showPreferences;
+export const validConsent = CookieConsent.validConsent;
+export const validCookie = CookieConsent.validCookie;
