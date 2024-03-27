2643b242484783f4c7c899cd980e4d94d3bca2d9
Pieter-Bas Kooij <pieter-bas.kooij@wearejust.com>
2024-03-27 14:30:00 +0100

    refactor: rewrite and restructure package

---
+++ src/constants/index.ts
@@ -0,0 +1,124 @@
+import { type CookieConsentConfig } from "vanilla-cookieconsent";
+
+export const defaultConfig = {
+  guiOptions: {
+    consentModal: {
+      layout: "box wide",
+      position: "bottom right",
+      equalWeightButtons: true,
+      flipButtons: true,
+    },
+    preferencesModal: {
+      layout: "box",
+      position: "right",
+      equalWeightButtons: true,
+      flipButtons: false,
+    },
+  },
+  categories: {
+    necessary: {
+      readOnly: true,
+    },
+    analytics: {},
+  },
+  language: {
+    default: "nl",
+    translations: {
+      nl: {
+        consentModal: {
+          title: "Deze website maakt gebruik van cookies",
+          description:
+            "Wij maken gebruik van functionele en analytische cookies om onze website te optimaliseren. Door gebruik te maken van deze website ga je akkoord met onze voorwaarden.",
+          acceptAllBtn: "Alles accepteren",
+          acceptNecessaryBtn: "Afwijzen",
+          showPreferencesBtn: "Voorkeuren beheren",
+          footer: `
+            <a href="#link">Privacybeleid</a>
+            <a href="#link">Algemene voorwaarden</a>
+          `,
+        },
+        preferencesModal: {
+          title: "Toestemmingen",
+          acceptAllBtn: "Alles accepteren",
+          acceptNecessaryBtn: "Alles afwijzen",
+          savePreferencesBtn: "Voorkeuren opslaan",
+          closeIconLabel: "Sluiten",
+          serviceCounterLabel: "Dienst|Diensten",
+          sections: [
+            {
+              title: "Cookiegebruik",
+              description:
+                "Deze website maakt gebruik van cookie technologie binnen de gestelde kaders van de AVG.",
+            },
+            {
+              title:
+                'Strikt noodzakelijke cookies <span class="pm__badge">Altijd ingeschakeld</span>',
+              description:
+                "Noodzakelijke cookies helpen een website bruikbaarder te maken, door basisfuncties als paginanavigatie en toegang tot beveiligde gedeelten van de website mogelijk te maken. Zonder deze cookies kan de website niet naar behoren werken.",
+              linkedCategory: "necessary",
+            },
+            {
+              title: "Analytische cookies",
+              description:
+                "Analytische cookies helpen eigenaren van websites begrijpen hoe bezoekers hun website gebruiken, door anoniem gegevens te verzamelen en te rapporteren.",
+              linkedCategory: "analytics",
+            },
+            {
+              title: "Marketing cookies",
+              description:
+                "Marketingcookies worden gebruikt om bezoekers op websites te volgen. De bedoeling is om advertenties weer te geven die relevant en aantrekkelijk zijn voor de individuele gebruiker en daardoor waardevoller voor uitgevers en externe adverteerders.",
+              linkedCategory: "marketing",
+            },
+          ],
+        },
+      },
+      en: {
+        consentModal: {
+          title: "This website uses cookies",
+          description:
+            "We use functional and analytical cookies to optimize our website. By using this website, you agree to our terms and conditions.",
+          acceptAllBtn: "Accept all",
+          acceptNecessaryBtn: "Reject all",
+          showPreferencesBtn: "Manage preferences",
+          footer:
+            '<a href="#link">Privacy Policy</a>\n<a href="#link">Terms and conditions</a>',
+        },
+        preferencesModal: {
+          title: "Consent Preferences",
+          acceptAllBtn: "Accept all",
+          acceptNecessaryBtn: "Reject all",
+          savePreferencesBtn: "Save preferences",
+          closeIconLabel: "Close",
+          serviceCounterLabel: "Service|Services",
+          sections: [
+            {
+              title: "Cookie Usage",
+              description:
+                "This website uses cookie technology within the established framework of the GDPR.",
+            },
+            {
+              title:
+                'Strictly Necessary Cookies <span class="pm__badge">Always Enabled</span>',
+              description:
+                "Necessary cookies help make a website more usable, by enabling basic functions such as page navigation and access to secure areas of the website. Without these cookies, the website cannot function properly.",
+              linkedCategory: "necessary",
+            },
+            {
+              title: "Analytics Cookies",
+              description:
+                "Analytical cookies help website owners understand how visitors use their website by anonymously collecting and reporting data.",
+              linkedCategory: "analytics",
+            },
+            {
+              title: "Marketing cookies",
+              description:
+                "Marketing cookies are used to track visitors across websites. The intention is to display ads that are relevant and engaging for the individual user and thereby more valuable for publishers and third party advertisers.",
+              linkedCategory: "marketing",
+            },
+          ],
+        },
+      },
+    },
+    autoDetect: "browser",
+  },
+} satisfies CookieConsentConfig;
---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import { type CookieConsentConfig } from "vanilla-cookieconsent";
+import * as CookieConsent from "vanilla-cookieconsent";
+import { merge } from "ts-deepmerge";
+
+import "vanilla-cookieconsent/dist/cookieconsent.css";
+
+import { defaultConfig } from "@/constants";
+import { DeepPartial } from "@/types";
+
+export const run = async (configuration: DeepPartial<CookieConsentConfig>) => {
+  const mergedConfiguration = merge(defaultConfig, configuration);
+  console.log({ configuration: mergedConfiguration });
+  await CookieConsent.run(mergedConfiguration as CookieConsentConfig);
+};
+
+export {
+  acceptCategory,
+  acceptedCategory,
+  acceptService,
+  acceptedService,
+  eraseCookies,
+  getConfig,
+  getCookie,
+  getUserPreferences,
+  hide,
+  hidePreferences,
+  loadScript,
+  reset,
+  setCookieData,
+  setLanguage,
+  show,
+  showPreferences,
+  validConsent,
+  validCookie,
+} from "vanilla-cookieconsent";
